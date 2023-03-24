73c91079f8f7df795940286b26851b9e2c2cd78a
Acer <ahmaded318@gmail.com>
2023-03-24 16:38:28 +0300

    3

--- app/src/main/java/com/example/bookexchange/MainActivity.java
+++ app/src/main/java/com/example/bookexchange/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.bookexchange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button button_sign,button_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_login=findViewById(R.id.button_log);
+        button_sign=findViewById(R.id.button_sign);
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+
+
+            }
+        });
+
+        button_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+
+
+            }
+        });
+
     }
 }
