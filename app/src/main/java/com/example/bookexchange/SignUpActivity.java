2ed35a3c06d3ef3235436c0efe7700f20d04e125
Acer <ahmaded318@gmail.com>
2023-03-24 16:45:48 +0300

    3

--- app/src/main/java/com/example/bookexchange/SignUpActivity.java
+++ app/src/main/java/com/example/bookexchange/SignUpActivity.java
@@ -9,6 +9,6 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_activity);
+        setContentView(R.layout.activity_signup);
     }
 }
