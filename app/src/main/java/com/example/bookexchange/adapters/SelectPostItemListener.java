4071b0a6992a87e6ec03976cef45ba23ef1a6d27
Abdelhadi99 <abdelhadialothman@gmail.com>
2023-05-20 15:18:26 +0300

    commit May 20

--- app/src/main/java/com/example/bookexchange/adapters/SelectPostItemListener.java
+++ app/src/main/java/com/example/bookexchange/adapters/SelectPostItemListener.java
@@ -1,16 +1,11 @@
 package com.example.bookexchange.adapters;
 
 import com.example.bookexchange.models.Post;
-import com.example.bookexchange.models.User;
-import com.google.firebase.auth.FirebaseUser;
 
 public interface SelectPostItemListener {
 
 
-    void onItemViewClicked(Post post);
-
-    void onItemTakeClicked(Post post, FirebaseUser currentUser);
-
+    void onItemClicked(Post post);
 
 
 }
