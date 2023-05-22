242ebe329d7899cde00e83177272ac7a81dbf84a
Abdelhadi99 <abdelhadialothman@gmail.com>
2023-05-22 19:24:33 +0300

    final check

--- app/src/main/java/com/example/bookexchange/ui/fragments/PostsFragment.java
+++ app/src/main/java/com/example/bookexchange/ui/fragments/PostsFragment.java
@@ -1,33 +1,53 @@
 package com.example.bookexchange.ui.fragments;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
-import androidx.activity.OnBackPressedDispatcher;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.bookexchange.adapters.PostAdapter;
 import com.example.bookexchange.R;
 import com.example.bookexchange.adapters.SelectPostItemListener;
+import com.example.bookexchange.dao.DAONotification;
 import com.example.bookexchange.dao.DAOPost;
+import com.example.bookexchange.models.Notification;
 import com.example.bookexchange.models.Post;
 import com.example.bookexchange.ui.activites.HomeActivity;
 import com.example.bookexchange.ui.activites.ViewPostActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 
@@ -36,19 +56,30 @@ public class PostsFragment extends Fragment implements SelectPostItemListener, S
 
     RecyclerView recyclerView;
     public static PostAdapter myadapter;
+    // Declare the launcher at the top of your Activity/Fragment:
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // FCM SDK (and your app) can post notifications.
+                } else {
+                    // TODO: Inform user that that your app will not show notifications.
+                }
+            });
+    FirebaseAuth auth;
     public static List<Post> postList = new ArrayList<>();
+
     DAOPost daoPost;
+    FirebaseUser user;
+    DAONotification daoNotification;
     SwipeRefreshLayout swipeRefreshLayout;
-
-
     private boolean fabShouldBeHidden = false; // Flag to keep track of FAB visibility
-
+    DatabaseReference databaseReference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.from(getContext()).inflate(R.layout.fragment_posts, container, false);
+
         fabShouldBeHidden = false;
-        daoPost = new DAOPost();
         SwipeRefreshLayout swipeRefreshLayout = view.findViewById(R.id.swipe);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -58,6 +89,10 @@ public class PostsFragment extends Fragment implements SelectPostItemListener, S
             }
         });
 
+
+
+        daoPost = new DAOPost();
+        daoNotification = new DAONotification();
         postList.clear();
 
 
@@ -76,19 +111,7 @@ public class PostsFragment extends Fragment implements SelectPostItemListener, S
 
 
                     postList.add(post);
-                    myadapter = new PostAdapter(requireContext(), postList, new SelectPostItemListener() {
-                        @Override
-                        public void onItemClicked(Post post) {
 
-
-                            Intent intent = new Intent(getContext(), ViewPostActivity.class);
-                            intent.putExtra("post", post);
-                            startActivity(intent);
-
-
-                        }
-                    });
-                    recyclerView.setAdapter(myadapter);
                     myadapter.notifyDataSetChanged();
 
 
@@ -96,12 +119,15 @@ public class PostsFragment extends Fragment implements SelectPostItemListener, S
 
             }
 
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
 
+        myadapter = new PostAdapter(requireContext(), postList, this);
+        recyclerView.setAdapter(myadapter);
 
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -117,20 +143,20 @@ public class PostsFragment extends Fragment implements SelectPostItemListener, S
 
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-//                // Get the last visible item position
-//                int lastVisibleItemPosition = ((LinearLayoutManager) recyclerView.getLayoutManager())
-//                        .findLastVisibleItemPosition();
-//
-//                // Check if the last visible item is the last item in the RecyclerView
-//                if (lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() - 1) {
-//                    // Hide the FAB and set flag to true
-//                    HomeActivity.fab.hide();
-//                    fabShouldBeHidden = true;
-//                } else {
-//                    // Set flag to false and show the FAB
-//                    fabShouldBeHidden = false;
-//                    HomeActivity.fab.show();
-//                }
+                // Get the last visible item position
+                int lastVisibleItemPosition = ((LinearLayoutManager) recyclerView.getLayoutManager())
+                        .findLastVisibleItemPosition();
+
+                // Check if the last visible item is the last item in the RecyclerView
+                if (lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() - 1) {
+                    // Hide the FAB and set flag to true
+                    HomeActivity.fab.hide();
+                    fabShouldBeHidden = true;
+                } else {
+                    // Set flag to false and show the FAB
+                    fabShouldBeHidden = false;
+                    HomeActivity.fab.show();
+                }
 
             }
         });
@@ -138,7 +164,23 @@ public class PostsFragment extends Fragment implements SelectPostItemListener, S
         return view;
     }
 
-
+    private void askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
+    }
 
 
     @Override
@@ -165,7 +207,84 @@ public class PostsFragment extends Fragment implements SelectPostItemListener, S
 
 
     @Override
-    public void onItemClicked(Post post) {
+    public void onItemViewClicked(Post post) {
+
+        Intent intent = new Intent(getContext(), ViewPostActivity.class);
+        intent.putExtra("post", post);
+        startActivity(intent);
+
 
     }
+
+
+    @Override
+    public void onItemTakeClicked(Post post, FirebaseUser currentUser) {
+
+        Date date = Calendar.getInstance().getTime();
+        String dateFormat = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(date);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference(Notification.class.getSimpleName());
+        String currentUserID = user.getUid();
+
+
+        if (!(post.getBookSellerId().equals(currentUser.getUid()))) {
+
+
+            Notification notification2 = new Notification(currentUser.getUid(), currentUser.getDisplayName(), post.getBookName(), dateFormat);
+
+
+            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    boolean notificationExists = false;
+
+
+                    for (DataSnapshot idSnapshot : snapshot.getChildren()) {
+                        if (idSnapshot.getKey().equals(post.getBookSellerId())) {
+                            for (DataSnapshot pushSnapshot : idSnapshot.getChildren()) {
+                                Notification notification = pushSnapshot.getValue(Notification.class);
+
+                                if (notification.getUserName().equals(user.getDisplayName()) && notification.getBookName().equals(post.getBookName())) {
+                                    // Notification already exists
+                                    notificationExists = true;
+                                    Toast.makeText(requireContext(), "You have already sent this notification before!", Toast.LENGTH_SHORT).show();
+                                    break;
+                                }
+                            }
+                            break; // Exit the loop once the specific ID is found
+                        }
+                    }
+
+                    if (!notificationExists) {
+                        daoNotification.add(notification2, post).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(requireContext(), "Notification added to DB", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(requireContext(), "Error", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // Handle onCancelled if needed
+                }
+            });
+
+
+        } else {
+            Toast.makeText(requireContext(), "You Can not Press Take to your Own Book !", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
 }
