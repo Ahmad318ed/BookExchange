401fa1c019ccee08004b729a02846615a7f41196
Abdelhadi99 <abdelhadialothman@gmail.com>
2023-05-28 19:44:50 +0300

    ViewPost changes May 28

--- app/src/main/java/com/example/bookexchange/ui/activites/Profile.java
+++ app/src/main/java/com/example/bookexchange/ui/activites/Profile.java
@@ -36,15 +36,21 @@ public class Profile extends AppCompatActivity {
     DAOProfileInfo daoProfile;
     ImageView img_profile;
 
-    ImageView btnFacebook, btnInstagram, btnWhatsApp;
-
-
+    String view_user_id;
     Profile_info load_profile_info;
+    private ImageView btnFacebook, btnInstagram, btnWhatsApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+        view_user_id = intent.getStringExtra("user_id");
+
+
+
+
 
         name = findViewById(R.id.username_profile);
         major = findViewById(R.id.major_profile);
@@ -64,7 +70,13 @@ public class Profile extends AppCompatActivity {
         username_Id = currentUser.getUid();
 
 
-        setDataToProfile();
+        if (view_user_id == null){
+            setDataToProfile();
+        }else{
+            setDataToProfile_FromViewActivity();
+        }
+
+
     }
 
 
@@ -201,27 +213,142 @@ public class Profile extends AppCompatActivity {
     }
 
 
+
+
+    private void setDataToProfile_FromViewActivity() {
+        daoProfile.get().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                for (DataSnapshot profilesnap : snapshot.getChildren()) {
+
+                    load_profile_info = profilesnap.getValue(Profile_info.class);
+
+                    //first if to check if the user is the same user that u take his data
+                    //Second if to check the contact info if it is empty or not then show it to the users
+
+
+
+                    if (view_user_id.equals(load_profile_info.getUserId())) {
+
+                        name.setText(load_profile_info.getName());
+                        major.setText(load_profile_info.getMajor());
+                        collage.setText(load_profile_info.getCollage());
+
+                        String WhatsApp =load_profile_info.getWhatsAppNum();
+                        String Facebook = load_profile_info.getFacebook_link();
+                        String Instagram = load_profile_info.getInstagram_link();
+
+
+
+
+                        if (!isDestroyed()) {
+                            Glide.with(Profile.this).load(load_profile_info.getImg()).fitCenter().centerCrop().into(img_profile);
+
+                        }
+
+
+                        //Second (3 ifs) to check the contact info if it is empty or not then show it to the users
+
+                        //check facebook link
+                        if (load_profile_info.getFacebook_link().equals("")) {
+                            btnFacebook.setClickable(false);
+                            btnFacebook.setAlpha(0.3f);
+
+                        }else if (!load_profile_info.getFacebook_link().contains("https://")) {
+                            btnFacebook.setAlpha(0.5f);
+                            btnFacebook.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Toast.makeText(Profile.this, "invalid facebook link", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+                            try {
+
+                                btnFacebook.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        GoToURL(Facebook);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                System.out.println("heeeeeelp" + e);
+                            }
+
+                        }
+
+
+
+
+                        //check Instagram link
+
+                        if (load_profile_info.getInstagram_link().equals("")) {
+                            btnInstagram.setClickable(false);
+                            btnInstagram.setAlpha(0.3f);
+
+                        } else if (!load_profile_info.getInstagram_link().contains("https://")) {
+                            btnInstagram.setAlpha(0.5f);
+                            btnInstagram.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Toast.makeText(Profile.this, "invalid instagram link", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+
+                            btnInstagram.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    GoToURL(Instagram);
+                                }
+                            });
+
+                        }
+
+
+
+                        //check whatsapp link
+                        if (load_profile_info.getWhatsAppNum().equals("")) {
+                            btnWhatsApp.setClickable(false);
+                            btnWhatsApp.setAlpha(0.3f);
+
+                        } else {
+                            btnWhatsApp.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    GoToURL(WhatsApp);
+                                }
+                            });
+                        }
+
+
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
+    }
+
+
+
+
+
     private void GoToURL(String url) {
         Uri uri = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
-
-//        try {
-//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-//            intent.setPackage("com.android.chrome"); // Package name of Chrome browser
-//
-//            // Verify that there's at least one activity that can handle the intent
-//            PackageManager packageManager = getPackageManager();
-//            if (intent.resolveActivity(packageManager) != null) {
-//                startActivity(intent);
-//            } else {
-//                // Handle the case where Chrome is not installed
-//                Toast.makeText(getApplicationContext(), "Chrome browser not installed", Toast.LENGTH_SHORT).show();
-//            }
-//        } catch (ActivityNotFoundException e) {
-//            // Handle the case where Chrome is not installed
-//            Toast.makeText(getApplicationContext(), "Chrome browser not installed", Toast.LENGTH_SHORT).show();
-//        }
     }
 
 }
