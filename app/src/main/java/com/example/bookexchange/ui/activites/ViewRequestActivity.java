6479449f6986aef83525cf1c2908853e3f264ffc
Abdelhadi99 <abdelhadialothman@gmail.com>
2023-06-04 00:08:59 +0300

    ViewRequest changes june 3

--- app/src/main/java/com/example/bookexchange/ui/activites/ViewRequestActivity.java
+++ app/src/main/java/com/example/bookexchange/ui/activites/ViewRequestActivity.java
@@ -1,16 +1,27 @@
 package com.example.bookexchange.ui.activites;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.bookexchange.R;
+import com.example.bookexchange.dao.DAOProfileInfo;
+import com.example.bookexchange.models.Profile_info;
 import com.example.bookexchange.models.Request;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class ViewRequestActivity extends AppCompatActivity {
 
@@ -18,6 +29,14 @@ public class ViewRequestActivity extends AppCompatActivity {
     ImageView user_image;
     FirebaseAuth auth;
     FirebaseUser user;
+
+    LinearLayout btnGoToProfile;
+
+    DAOProfileInfo daoProfile;
+    Profile_info load_profile_info;
+    String userId;
+    private ImageView btnFacebook, btnInstagram, btnWhatsApp;
+
 
 
 
@@ -32,6 +51,8 @@ public class ViewRequestActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
+        daoProfile = new DAOProfileInfo();
+
 
         Request request=(Request) getIntent().getSerializableExtra("request");
 
@@ -39,10 +60,62 @@ public class ViewRequestActivity extends AppCompatActivity {
 
          String username=request.getBookSellerName();
          String userEmail=request.getBookSellerEmail();
-         String userId=request.getBookSellerId();
+
+          userId=request.getBookSellerId();
+
+        // To send the user to Profile screen
+        ///////////////////////////////////////
+
+        btnGoToProfile = findViewById(R.id.btnGoToProfile);
+        btnGoToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ViewRequestActivity.this,Profile.class);
+                intent.putExtra("user_id",userId);
+                startActivity(intent);
+            }
+        });
 
 
-         user_image.setImageURI(user.getPhotoUrl());
+
+        daoProfile.get().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                for (DataSnapshot profilesnap : snapshot.getChildren()) {
+
+                    Profile_info profile = profilesnap.getValue(Profile_info.class);
+
+
+
+                    if (userId.equals(profile.getUserId())) {
+
+                        if (profile.getImg() != null){
+                            Glide.with(ViewRequestActivity.this).load(profile.getImg()).fitCenter().centerCrop().into(user_image);
+
+                        }else{
+                            Glide.with(ViewRequestActivity.this).load(getDrawable(R.drawable.default_profile_img)).fitCenter().centerCrop().into(user_image);
+
+                        }
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        ///////////////////////////////////////////////////////
+
+
+
+
         tv_username.setText(username);
         tv_user_email.setText(userEmail);
         tv_book_name.setText(request.getBookName());
@@ -53,8 +126,7 @@ public class ViewRequestActivity extends AppCompatActivity {
         tv_book_details.setText(request.getBookDetails());
 
 
-
-
+        setContacts();
 
 
     }
@@ -70,10 +142,139 @@ public class ViewRequestActivity extends AppCompatActivity {
         tv_book_edition=findViewById(R.id.book_edition_view_request);
         tv_book_states=findViewById(R.id.book_states_view_request);
         tv_book_price=findViewById(R.id.book_price_view_request);
+        btnFacebook = findViewById(R.id.btn_imgFacebook);
+        btnInstagram = findViewById(R.id.btn_imgInstagram);
+        btnWhatsApp = findViewById(R.id.btn_imgWhatsApp);
 
 
 
     }
+
+
+
+    private void setContacts() {
+        daoProfile.get().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                for (DataSnapshot profilesnap : snapshot.getChildren()) {
+
+                    load_profile_info = profilesnap.getValue(Profile_info.class);
+
+                    //first if to check if the user is the same user that u take his data
+                    //Second if to check the contact info if it is empty or not then show it to the users
+
+
+
+                    if (userId.equals(load_profile_info.getUserId())) {
+
+
+                        String WhatsApp =load_profile_info.getWhatsAppNum();
+                        String Facebook = load_profile_info.getFacebook_link();
+                        String Instagram = load_profile_info.getInstagram_link();
+
+
+
+                        //Second (3 ifs) to check the contact info if it is empty or not then show it to the users
+
+                        //check facebook link
+                        if (load_profile_info.getFacebook_link().equals("")) {
+                            btnFacebook.setClickable(false);
+                            btnFacebook.setAlpha(0.3f);
+
+                        }else if (!load_profile_info.getFacebook_link().contains("https://")) {
+                            btnFacebook.setAlpha(0.5f);
+                            btnFacebook.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Toast.makeText(ViewRequestActivity.this, "invalid facebook link", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+                            try {
+
+                                btnFacebook.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        GoToURL(Facebook);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                System.out.println("heeeeeelp" + e);
+                            }
+
+                        }
+
+
+
+
+                        //check Instagram link
+
+                        if (load_profile_info.getInstagram_link().equals("")) {
+                            btnInstagram.setClickable(false);
+                            btnInstagram.setAlpha(0.3f);
+
+                        } else if (!load_profile_info.getInstagram_link().contains("https://")) {
+                            btnInstagram.setAlpha(0.5f);
+                            btnInstagram.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Toast.makeText(ViewRequestActivity.this, "invalid instagram link", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+
+                            btnInstagram.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    GoToURL(Instagram);
+                                }
+                            });
+
+                        }
+
+
+
+                        //check whatsapp link
+                        if (load_profile_info.getWhatsAppNum().equals("")) {
+                            btnWhatsApp.setClickable(false);
+                            btnWhatsApp.setAlpha(0.3f);
+
+                        } else {
+                            btnWhatsApp.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    GoToURL(WhatsApp);
+                                }
+                            });
+                        }
+
+
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
+    }
+
+
+    private void GoToURL(String url) {
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
 
     @Override
     public void onBackPressed() {
